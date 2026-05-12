aa736b65b2343d14a4a28980b7dd4ff95b8b8c1c
client1 <kaybeelateef@gmail.com>
2026-05-12 05:06:49 +0000

    script M

--- system-info-script/monitor_agent.sh
+++ system-info-script/monitor_agent.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 cpu_load=$(uptime | cut -d':' -f5)
+memory=$(free -h | awk 'NR==2 {print $3 "/" $2}')
 
 while true
 do
+	echo "======================="
+	echo
 	echo "$(date)"
-	echo "cpu-load: $cpu_load"
+	echo
+	echo "Cpu-load: $cpu_load"
+	echo "Memory usage $memory"
+	sleep 5 
 done
