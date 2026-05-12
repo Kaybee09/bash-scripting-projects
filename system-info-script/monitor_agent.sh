344d7cff299bfc67d0878ba8cf50c0184ca6c296
client1 <kaybeelateef@gmail.com>
2026-05-12 05:37:30 +0000

    script M

--- system-info-script/monitor_agent.sh
+++ system-info-script/monitor_agent.sh
@@ -2,6 +2,7 @@
 
 cpu_load=$(uptime | cut -d':' -f5)
 memory=$(free -h | awk 'NR==2 {print $3 "/" $2}')
+disk=$(df / | awk 'NR==2 {print $5}')
 
 while true
 do
@@ -11,5 +12,11 @@ do
 	echo
 	echo "Cpu-load: $cpu_load"
 	echo "Memory usage $memory"
+	echo "Disk Usage $disk"
+	echo "======================"
+	echo
+	echo "STATUS OK"
 	sleep 5 
 done
+
+
