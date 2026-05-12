34d9af27787c3232fd15d8d840709bd523a92ab6
client1 <kaybeelateef@gmail.com>
2026-05-12 18:23:07 +0000

    Script M

--- system-info-script/monitor_agent.sh
+++ system-info-script/monitor_agent.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-cpu_load=$(uptime | cut -d':' -f5)
-memory=$(free -h | awk 'NR==2 {print $3 "/" $2}')
-disk=$(df / | awk 'NR==2 {print $5}' | sed 's/%//')
-
 while true
 do
+	cpu_load=$(uptime | awk -F'load average:' '{print $2}')
+	memory=$(free -h | awk 'NR==2 {print $3 "/" $2}')
+	disk=$(df / | awk 'NR==2 {print $5}' | sed 's/%//')
+
+
 	echo "======================="
 	echo
 	echo "$(date)"
@@ -16,10 +17,10 @@ do
 	echo "======================"
 	echo
 	if [ $disk -gt 80 ]; then
-        echo "WARNING‼️ ⛔️ disk is high"
+        echo "WARNING‼️ Disk is higher than 80%"
 else
         echo "STATUS OK"
 fi
 	
-	sleep 5 
+	sleep 10 
 done >> system_health.log
