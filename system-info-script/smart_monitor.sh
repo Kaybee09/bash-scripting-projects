58166a2571f4ea4dd7e11a169a96cbb3aee3d22a
client1 <kaybeelateef@gmail.com>
2026-05-13 15:17:26 +0000

    Script m

--- system-info-script/smart_monitor.sh
+++ system-info-script/smart_monitor.sh
@@ -2,29 +2,38 @@
 
 
 server=$(hostname)
-os_info=$(cat /etc/os-release | grep -i "24")
-cpu_load=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+. /etc/os-release
+os_info=$PRETTY_NAME
+cpu_load=$(awk '{print $1}' /proc/loadavg)
 
 echo "[INFO]"
 echo "=================="
 echo 
-echo "SERVER: $server"
-echo "$os_info"
+echo "SERVER:$server"
+echo "OS:$os_info"
 echo
 echo "CPU STATUS"
 echo "================="
-if [ "$cpu_load" -ge 80 ]; then
-	echo "Monitoring CPU, Please wait...."
-	sleep 5
-	echo "CRITICAL: Very High CPU load"
-elif [ "$cpu_load" -ge 50 ]; then
-	echo "Monitoring CPU, Please wait...."
-	echo "WARNING: High CPU load"
+
+echo "Load Average (1 min): $cpu_load"
+
+if (( $(echo "$cpu_load > 2.0" | bc -l) )); then
+    echo "CRITICAL: Very High Load"
+elif (( $(echo "$cpu_load > 1.0" | bc -l) )); then
+    echo "WARNING: High Load"
 else
-	echo "Monitoring CPU, Please wait...."
-	sleep 5
-	echo "CPU STATUS IS OK"
-	echo "Monitoring stopped"
+    echo "CPU STATUS IS OK"
 fi
+
+
+
+
+
+
+
+
+
+
+
 
 
