b11b3ea62289c1ddd5bb127c7dbbe1eae2d73605
client1 <kaybeelateef@gmail.com>
2026-05-14 07:48:51 +0200

    Script M

--- system-info-script/smart_monitor.sh
+++ system-info-script/smart_monitor.sh
@@ -1,39 +1,84 @@
 #!/bin/bash
 
+cleanup() {
+    echo
+    echo "=================="
+    echo "Exiting monitoring script safely..."
+    echo "Goodbye!"
+    exit 0
+}
 
-server=$(hostname)
-. /etc/os-release
-os_info=$PRETTY_NAME
-cpu_load=$(awk '{print $1}' /proc/loadavg)
+trap cleanup SIGINT SIGTERM
 
-echo "[INFO]"
-echo "=================="
-echo 
-echo "SERVER:$server"
-echo "OS:$os_info"
-echo
-echo "CPU STATUS"
-echo "================="
+while true; do
 
-echo "Load Average (1 min): $cpu_load"
+    clear
 
-if (( $(echo "$cpu_load > 2.0" | bc -l) )); then
-    echo "CRITICAL: Very High Load"
-elif (( $(echo "$cpu_load > 1.0" | bc -l) )); then
-    echo "WARNING: High Load"
-else
-    echo "CPU STATUS IS OK"
-fi
+    server=$(hostname)
 
+    . /etc/os-release
+    os_info=$PRETTY_NAME
 
+    cpu_load=$(awk '{print $1}' /proc/loadavg)
 
+    echo "[INFO]"
+    echo "=================="
+    echo
+    echo "SERVER: $server"
+    echo "OS: $os_info"
+    echo
+    echo "CPU STATUS"
+    echo "================="
 
+    echo "Load Average (1 min): $cpu_load"
 
+    if (( $(echo "$cpu_load > 2.0" | bc -l) )); then
+        echo "CRITICAL: Very High Load"
+    elif (( $(echo "$cpu_load > 1.0" | bc -l) )); then
+        echo "WARNING: High Load"
+    else
+        echo "CPU STATUS IS OK"
+    fi
 
+    sleep 2
 
+done
 
-
-
-
-
-
+#
+#
+#server=$(hostname)
+#. /etc/os-release
+#os_info=$PRETTY_NAME
+#cpu_load=$(awk '{print $1}' /proc/loadavg)
+#
+#echo "[INFO]"
+#echo "=================="
+#echo 
+#echo "SERVER:$server"
+#echo "OS:$os_info"
+#echo
+#echo "CPU STATUS"
+#echo "================="
+#
+#echo "Load Average (1 min): $cpu_load"
+#
+#if (( $(echo "$cpu_load > 2.0" | bc -l) )); then
+#    echo "CRITICAL: Very High Load"
+#elif (( $(echo "$cpu_load > 1.0" | bc -l) )); then
+#    echo "WARNING: High Load"
+#else
+#    echo "CPU STATUS IS OK"
+#fi
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
