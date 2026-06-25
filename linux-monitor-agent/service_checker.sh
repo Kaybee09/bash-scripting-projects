046cd88791f41f830e7fe10e54df1330ba03baa2
kaybee09 <kaybeelateef@gmail.com>
2026-06-25 21:33:15 +0200

    v2.0.0

--- linux-monitor-agent/service_checker.sh
+++ linux-monitor-agent/service_checker.sh
@@ -12,8 +12,9 @@ echo "Service Check - $(date)" >> "$LOG_FILE"
 for service in "$@"
 do
     if systemctl is-active --quiet "$service"; then
-        echo "$service is running"
-        echo "$(date) - OK - $service is running" >> "$LOG_FILE"
+       message="$(date) - OK - $service is running"
+        echo "$message"
+        echo "$message" >> "$LOG_FILE"
     else
         echo "$service is not running"
         echo "$(date) - FAILED - $service is not running" >> "$LOG_FILE"
